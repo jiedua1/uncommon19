9517d8d494b8a7dd318bf1d350734e718ef1d5ef
jiedua1 <jiedongduan@gmail.com>
2019-02-16 16:04:06 -0600

    basic code a few lines screenshot

--- visionTest.py
+++ visionTest.py
@@ -1,3 +1,19 @@
+from PIL import ImageGrab
+import win32gui
+import os
+import time
 import cv2
+import pyautogui
 
-print("hello")
+def screenGrab():
+    box = ()
+    im = ImageGrab.grab()
+    for i in range(3):
+        im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', 'PNG')
+        im.show()
+
+def main():
+    screenGrab()
+
+if __name__ == '__main__':
+    main()
